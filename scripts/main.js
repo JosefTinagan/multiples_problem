c67d9616d898ecc03dfb9de5f87635bd5f8af3d4
TinaganJosef <josephperry_06@yahoo.com>
2016-09-06 18:07:49 +0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+//prompt("Till what number should we check?")
+/*
+var sum = 0;
+var list = [];
+var it = 0;
+
+
+var numLength = prompt("Till what number should we check?: ")
+
+for( i = 0 ; i < numLength ; i++){
+		
+	if ( i % 3 === 0 || i % 5 === 0){
+		sum += i;
+		list[it] = i;
+		it ++;
+		
+	}
+}
+
+console.log(list);
+console.log(sum);
+*/
